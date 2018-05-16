83eee68b0b8d968da0e2b3f113e9a88ad3ed7c11
skoulouzis <S.Koulouzis@uva.nl>
2018-05-16 21:17:16 +0200

    read  from json

--- senders/rabbitmq_sender.py
+++ senders/rabbitmq_sender.py
@@ -1,8 +1,10 @@
 from senders.sender import *
+import json
 
 class RabbitmqSender(Sender):
         
-    def send(self, grafana_alert):
+    def send(self, grafana_alert_message):
+        grafana_alert = json.loads(grafana_alert_message)    
         print grafana_alert
         print grafana_alert["ruleId"]
         print grafana_alert["state"]
